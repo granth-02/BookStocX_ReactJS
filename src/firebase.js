f70e3b01cc5bb8a1689756f119d84f933a9702f4
granth <75667699+granth-02@users.noreply.github.com>
2023-08-06 11:17:08 +0530

    commit

--- src/firebase.js
+++ src/firebase.js
@@ -3,22 +3,23 @@ import { getAnalytics } from "firebase/analytics";
 import { getAuth, GoogleAuthProvider } from "firebase/auth"
 import { getFirestore } from "firebase/firestore";
 
+
 const firebaseConfig = {
   apiKey: "AIzaSyChF_tzUGMHmXy2PILdABEwGTuTmei0V9w",
   authDomain: "bookstocx-ebf6a.firebaseapp.com",
   projectId: "bookstocx-ebf6a",
   storageBucket: "bookstocx-ebf6a.appspot.com",
   messagingSenderId: "757238555486",
-  appId: "1:757238555486:web:934095403e2954e7fd25cf",
-  measurementId: "G-8N5JD8VFTC"
+  appId: "1:757238555486:web:739d30dfcff16f54fd25cf",
+  measurementId: "G-EFKR7211X7"
 };
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
-const db = getFirestore(app);
+const db = getFirestore(app)
 
 export { auth, provider}
 
-export default db;
+// export default db;
